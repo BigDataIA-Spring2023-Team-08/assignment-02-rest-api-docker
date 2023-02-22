258d8fac6d3d6eb49f08ef45d5189b9e9661388b
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-22 04:19:57 -0600

    added code to create new user and password in db

--- auth_main.py
+++ auth_main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from userdb import engine, SessionLocal
+import db_model
+from sqlalchemy.orm import Session
 
-app =FastAPI()
+app = FastAPI()
+
+db_model.Base.metadata.create_all(bind = engine)
 
 class User(BaseModel):
     name: str
-    email: str
     password: str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
 @app.post('/user')
-def create_user(request: User):
-    return request
+def create_user(request: User, db: Session = Depends(get_db)):
+    new_user = db_model.User_Table(name = request.name, password = request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
 
