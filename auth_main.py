0aea68693b26864fe616f913b7376d500d52970c
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-23 00:22:55 -0600

    updated auth main and user database files

--- auth_main.py
+++ auth_main.py
@@ -2,54 +2,45 @@ from collections import UserList
 from http.client import HTTPException
 from fastapi import FastAPI, Depends, status
 from pydantic import BaseModel
-from passlib.context import CryptContext
-from userdb import engine, SessionLocal
+from sympy import im
+from userdb import engine, get_db
 import db_model
 from sqlalchemy.orm import Session
+from hashing import Hash
+from router import user,authenticate
 
 
 app = FastAPI()
 
 db_model.Base.metadata.create_all(bind = engine)
 
-class User(BaseModel):
-    name: str
-    password: str
+app.include_router(user.router)
+app.include_router(authenticate.router)
 
-class ShowUser(BaseModel):
-    name: str
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close
 
-    class Config():
-        orm_mode = True
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close
+# @app.post('/user', response_model= ShowUser,tags=['Users'])
+# def create_user(request: User, db: Session = Depends(get_db)):
+#     new_user = db_model.User_Table(name = request.name, password = Hash.bcrypt(request.password))
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+#     return new_user
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated= "auto")
-class Hash():
-    def bcrypt(password: str):
-       return  pwd_context.hash(password)
+# @app.get('/user/{id}',response_model=ShowUser,tags=['Users'])
+# def get_user(id:int, db: Session = Depends(get_db)):
+#     user = db.query(db_model.User_Table).filter(db_model.User_Table.id == id).first()
 
-@app.post('/user', response_model= ShowUser,tags=['Users'])
-def create_user(request: User, db: Session = Depends(get_db)):
-    new_user = db_model.User_Table(name = request.name, password = Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@app.get('/user/{id}',response_model=ShowUser,tags=['Users'])
-def get_user(id:int, db: Session = Depends(get_db)):
-    user = db.query(db_model.User_Table).filter(db_model.User_Table.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = f'User with id{id} is not available')
+#     if not user:
+#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+#                             detail = f'User with id{id} is not available')
                 
-    return user
+#     return user
 
 
