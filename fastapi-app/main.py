7cf2fb1fa143309741a3357366828e31b8e6e0f3
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-23 16:28:02 -0500

    removed get db file

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlite3 import Connection
 import os
 from dotenv import load_dotenv
-from get_database_file import get_database_file
+#from get_database_file import get_database_file
 import boto3
 import pandas as pd
 from routers import database, s3, fetch_file
