64e79c9309ab3504d58d16d74909026980c08c57
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-23 17:09:34 -0500

    removed download

--- fastapi-app/get_database_file.py
+++ fastapi-app/get_database_file.py
@@ -15,11 +15,12 @@ s3client = boto3.client('s3',
                         aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
                         aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
                         )
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
 
-user_bucket = os.environ.get('USER_BUCKET_NAME')
-s3client.download_file(user_bucket, 'database-files/sql_scraped_database.db', f"{dir_path}/sql_scraped_database.db")
+# user_bucket = os.environ.get('USER_BUCKET_NAME')
+
+# s3client.download_file(user_bucket, 'database-files/sql_scraped_database.db', f"{dir_path}/sql_scraped_database.db")
 
 async def get_database_file():
     database_connection = sqlite3.connect('sql_scraped_database.db')
