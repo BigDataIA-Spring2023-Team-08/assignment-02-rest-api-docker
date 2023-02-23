bde12733098d05cefbeaffc4f999e411120f4926
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-23 01:06:38 -0600

    removed commented code from auth main

--- auth_main.py
+++ auth_main.py
@@ -1,46 +1,15 @@
-from collections import UserList
-from http.client import HTTPException
-from fastapi import FastAPI, Depends, status
-from pydantic import BaseModel
-from sympy import im
-from userdb import engine, get_db
+from fastapi import FastAPI
+from userdb import engine
 import db_model
-from sqlalchemy.orm import Session
-from hashing import Hash
 from router import user,authenticate
 
 
 app = FastAPI()
 
-db_model.Base.metadata.create_all(bind = engine)
+db_model.Base.metadata.create_all(bind = engine) #create all tables stored in db if not present
 
-app.include_router(user.router)
-app.include_router(authenticate.router)
+app.include_router(user.router) #route to creating user 
+app.include_router(authenticate.router) #route to login 
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close
-
-
-# @app.post('/user', response_model= ShowUser,tags=['Users'])
-# def create_user(request: User, db: Session = Depends(get_db)):
-#     new_user = db_model.User_Table(name = request.name, password = Hash.bcrypt(request.password))
-#     db.add(new_user)
-#     db.commit()
-#     db.refresh(new_user)
-#     return new_user
-
-# @app.get('/user/{id}',response_model=ShowUser,tags=['Users'])
-# def get_user(id:int, db: Session = Depends(get_db)):
-#     user = db.query(db_model.User_Table).filter(db_model.User_Table.id == id).first()
-
-#     if not user:
-#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-#                             detail = f'User with id{id} is not available')
-                
-#     return user
 
 
