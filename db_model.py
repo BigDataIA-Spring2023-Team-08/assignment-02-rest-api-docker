fb9549e2295017eb71aa9d92f81e05d19001668f
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-24 11:21:48 -0600

    adding comments

--- db_model.py
+++ db_model.py
@@ -1,10 +1,10 @@
 from userdb import Base
 from sqlalchemy import Column, Integer, String
 
-class User_Table(Base):
-    __tablename__ = "users"
+class User_Table(Base):   #class to create schema for the table of users in db
+    __tablename__ = "users"  
     
     id = Column(Integer, primary_key= True, index=True)
     name = Column(String)
-    #email = Column(String)
+    username = Column(String)
     password = Column(String)
