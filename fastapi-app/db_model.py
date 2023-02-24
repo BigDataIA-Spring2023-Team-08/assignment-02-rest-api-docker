f878b9744f7efdfc7aba1450a8207236e80f8b28
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-24 14:57:22 -0500

    db model

--- fastapi-app/db_model.py
+++ fastapi-app/db_model.py
@@ -1,8 +1,8 @@
 from userdb import Base
 from sqlalchemy import Column, Integer, String
 
-class User_Table(Base):   #class to create schema for the table of users in db
-    __tablename__ = "users"  
+class User_Table(Base):
+    __tablename__ = "all_users"
     
     id = Column(Integer, primary_key= True, index=True)
     name = Column(String)
