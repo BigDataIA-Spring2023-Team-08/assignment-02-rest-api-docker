2583a556624202cbc96187c519157752567602f1
Anushka Desai <anushkadesai077@gmail.com>
2023-02-24 15:01:02 -0500

    get database file added

--- fastapi-app/get_database_file.py
+++ fastapi-app/get_database_file.py
@@ -15,13 +15,15 @@ s3client = boto3.client('s3',
                         aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
                         aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
                         )
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# print(dir_path)
 
-# user_bucket = os.environ.get('USER_BUCKET_NAME')
+user_bucket = os.environ.get('USER_BUCKET_NAME')
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# s3client.download_file(user_bucket, 'database-files/sql_scraped_database.db', f"{dir_path}/sql_scraped_database.db")
+if (os.environ.get('CI_FLAG')==True):
+    pass    #to allow testing CI via github actions, set the variable through github
+else:   #else download the file stored by the airflow dag from the s3 bucket 
+    s3client.download_file(user_bucket, 'database-files/sql_scraped_database.db', f"{dir_path}/sql_scraped_database.db")
 
 async def get_database_file():
-    database_connection = sqlite3.connect('sql_scraped_database.db')
+    database_connection = sqlite3.connect('sql_scraped_database.db')    #connect to metadata db
     return database_connection
