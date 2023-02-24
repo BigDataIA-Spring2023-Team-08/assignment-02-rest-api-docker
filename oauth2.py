321dc8ca1a28fe807de5bcb9308f9a7532080bb4
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-24 13:07:18 -0600

    updated oauth2 with comments

--- oauth2.py
+++ oauth2.py
@@ -4,6 +4,17 @@ from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "login")
 def get_current_user(token: str = Depends(oauth2_scheme)):
+    """Function to get the current user in session by verifying their JWT token. 
+    Raises exception if the token does not match or if user does not exist
+    -----
+    Input parameters:
+    token : str
+        string token depending on the current user in session
+    -----
+    Returns:
+    pwd_context.hash(password) : bool
+        True if token is verified, Raises 401_unauthorized exception if False
+    """
     credentials_exception = HTTPException(
         status_code= status.HTTP_401_UNAUTHORIZED,
         detail = "Could not validate credentials",
@@ -11,4 +22,3 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     )
 
     return JWTToken.verify_token(token, credentials_exception)
-
