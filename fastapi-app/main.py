c7c7705c59bec8ed44cd95c6522dbfd42dc9f791
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-24 14:53:46 -0500

    fastapi main & req

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI, APIRouter, status, HTTPException, Depends
 import sqlite3
-from sqlite3 import Connection
 import os
-from dotenv import load_dotenv
-#from get_database_file import get_database_file
 import boto3
 import pandas as pd
-from routers import database, s3, fetch_file
+import db_model
+from fastapi import FastAPI, APIRouter, status, HTTPException, Depends
+from dotenv import load_dotenv
+from sqlite3 import Connection
+from get_database_file import get_database_file
+from userdb import engine
+from routers import database, s3, fetchfile, user, authenticate
 
 #load env variables
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI()     #create fastapi object
 
+db_model.Base.metadata.create_all(bind = engine) #create all tables stored in db if not present
+
+#add all routers
 app.include_router(database.router)
 app.include_router(s3.router)
-app.include_router(fetch_file.router)
+app.include_router(fetchfile.router)
+app.include_router(user.router)
+app.include_router(authenticate.router)
