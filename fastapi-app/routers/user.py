472a6f0b2cdbede9cbd2dbcc82a0faa794bf0dab
Poojitha Konduparti <91445200+poojithakonduparti@users.noreply.github.com>
2023-02-24 13:02:18 -0600

    updated authenticate and user routes with comments

--- fastapi-app/routers/user.py
+++ fastapi-app/routers/user.py
@@ -11,20 +11,23 @@ router = APIRouter(
 
 get_db = userdb.get_db
 
-@router.post('/user', response_model= schema.ShowUser)
-def create_user(request: schema.User, db: Session = Depends(get_db),current_user: schema.User = Depends(oauth2.get_current_user)):
-    new_user = db_model.User_Table(name = request.name, password = Hash.bcrypt(request.password))
-    db.add(new_user)
+@router.post('/user/create', response_model= schema.ShowUser)
+def create_user(request: schema.User, db: Session = Depends(get_db)):
+    """Creates a User in the User_Table inside the SQLite DB. The function stores the Name, Username and
+        hashed password in the table to maintain privacy of the user.
+    -----
+    Input parameters:
+    file_name : str
+        string containg the filename (including extensions, if any) to fetch URL for
+    Session : db
+        current session of db
+    -----
+    Returns:
+    new_user : db
+        new user entry in the User_Table of the database
+    """
+    new_user = db_model.User_Table(name = request.name, username = request.username, password = Hash.bcrypt(request.password)) #creates a new user 
+    db.add(new_user) 
     db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@router.get('/user/{id}',response_model=schema.ShowUser)
-def get_user(id:int, db: Session = Depends(get_db),current_user: schema.User = Depends(oauth2.get_current_user)):
-    user = db.query(db_model.User_Table).filter(db_model.User_Table.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = f'User with id{id} is not available')
-                
-    return user
+    db.refresh(new_user) #new user in the existing table
+    return new_user  
