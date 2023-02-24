104728c271f4149d4bb885695438b0dabf23cdbc
Vraj Mashruwala <vrajmashruwala09@gmail.com>
2023-02-24 15:20:05 -0500

    Update get_database_file.py

--- fastapi-app/get_database_file.py
+++ fastapi-app/get_database_file.py
@@ -19,7 +19,7 @@ s3client = boto3.client('s3',
 user_bucket = os.environ.get('USER_BUCKET_NAME')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-if (os.environ.get('CI_FLAG')==True):
+if (os.environ.get('CI_FLAG')=='True'):
     pass    #to allow testing CI via github actions, set the variable through github
 else:   #else download the file stored by the airflow dag from the s3 bucket 
     s3client.download_file(user_bucket, 'database-files/sql_scraped_database.db', f"{dir_path}/sql_scraped_database.db")
